46a1ddad6afc747cb9776cb5c804b75ee437c413
NepThunder <tekchau21@gmail.com>
2024-02-27 19:32:32 +0530

    fileupload working

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,18 +1,48 @@
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/router";
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
 import { trpc } from "../_trpc/client";
+import { Loader2 } from "lucide-react";
 
-const Page=async()=>{
+const Page = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const origin = searchParams.get("origin");
 
-    const router=useRouter()
-    const searchParams=useSearchParams()
-    const origin=searchParams.get('origin')
-    // const apiResponse=await fetch('api/whatever')
-    // const data=await apiResponse.json()
-    const {data,isLoading}= trpc.authCallBack.useQuery(undefined);
-    if (!isLoading && data && data.success) {
-        router.push(origin ? `/${origin}` : '/dashboard');
+  const { data, isLoading, error } = trpc.authCallBack.useQuery(undefined, {
+    retry: true,
+    retryDelay: 500,
+  });
+
+  // Handle loading state
+  if (isLoading) {
+    return (
+      <div className="w-full mt-24 flex justify-center">
+        <div className="flex flex-col items-center gap-2">
+          <Loader2 className="h-8 w-8 animate-spin text-zinc-800" />
+          <h3 className="font-semibold text-xl">Setting up your account...</h3>
+          <p>You will be redirected automatically.</p>
+        </div>
+      </div>
+    );
+  }
+
+  // Handle error state
+  if (error) {
+    if (error.data?.code === "UNAUTHORIZED") {
+      router.push("/sign-in");
+    } else {
+      console.error("Error:", error);
     }
-    
-}
+    return null;
+  }
+
+  // Handle success state
+  if (data && data.success) {
+    router.push(origin ? `/${origin}` : "/dashboard");
+  }
+
+  // If none of the above conditions are met, render null or a fallback component
+  return null;
+};
 export default Page;
